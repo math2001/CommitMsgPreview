13656b7c217012d20d5fe41ec032b7999d7a65fd
Mathieu PATUREL <australie.p@gmail.com>
2017-03-18 11:57:55 +1100

    :sparkles: preview working

--- CommitMsgPreview.py
+++ CommitMsgPreview.py
@@ -3,12 +3,14 @@
 import sublime
 import sublime_plugin
 import re
+import os
+import json
 
 phantom_sets = {
     # id: phantom_sets
 }
 
-EMOJI_ALIASES = re.compile(r':.+?:')
+EMOJI_ALIASES = re.compile(r':[a-zA-Z]+?:')
 
 class CommitMsgPreview(sublime_plugin.EventListener):
 
@@ -18,16 +20,21 @@ class CommitMsgPreview(sublime_plugin.EventListener):
 
         first_line = view.line(0)
         text = view.substr(first_line)
-        text = EMOJI_ALIASES.sub('?', text)
-        length = len(text)
+
+        html = text.replace(' ', '&nbsp;')
+        with open(os.path.join(__file__, '..', 'emojis.json')) as fp:
+            for alias, base64 in json.load(fp).items():
+                html = html.replace(':{}:'.format(alias),
+                                    '<img src="data:image/png;base64,{0}" height="{1}" width="{1}"/>'.format(base64, view.line_height()))
+
+        length = len(EMOJI_ALIASES.sub(' ', text))
         color = 'var(--' + ('greenish' if length <= 50 else 'redish')
-        text = text.replace(' ', '&nbsp;')
-        text += ' <span style="color: {}">| {}</span>'.format(color, length)
+        html += ' <span style="color: {}"> → {}</span>'.format(color, length)
 
         view_id = view.id()
 
         ps = phantom_sets.get(view_id) or sublime.PhantomSet(view, 'commit-msg-preview')
         phantom_sets[view_id] = ps
         ps.update([
-            sublime.Phantom(first_line, text, sublime.LAYOUT_BLOCK)
+            sublime.Phantom(first_line, html, sublime.LAYOUT_BLOCK)
         ])
--- README.md
+++ README.md
@@ -11,5 +11,8 @@ actual character, the ruler that I set doesn't work any more.
 
 So, this plugin just shows the actual line plus it's length under the first one *only*.
 
+The emojis are coming from the awesome [emojione][]
+
 [gitmoji guide]: https://gitmoji.carloscuesta.me/
 [1]: https://github.com/search?utf8=%E2%9C%93&q=topic%3Agitmoji+user%3Amath2001&type=Repositories&ref=searchresults
+[emojione]: https://emojione.com
